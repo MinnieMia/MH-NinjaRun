5aec94a776df0d2f986c097d3d782e6fbc086287
BillySwCodeNinjas <130806226+MinnieMia@users.noreply.github.com>
2024-01-13 13:54:43 -0600

    I did it!!!

---
+++ KO2-PY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    bool hit;
+    int falseLayer = 1 << 8;
+   
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, transform.forward, 3, falseLayer);
+        if (hit)
+        {
+            Debug.LogWarning("Be Careful!");
+        }
+        else
+        {
+            Debug.Log("All clear");
+        }
+    }
+}
---
+++ MH-NinjaRun/Assets/Scripts/Pickups.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+
+    public int score;
+    public Text scoreText;
+
+    public ParticleSystem Pickup;
+
+    void Start()
+    {
+       Pickup.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("coin")){
+            //if the other game object has the tag coin, then do this...
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+
+            Pickup.Play();
+        }
+    }
+}
